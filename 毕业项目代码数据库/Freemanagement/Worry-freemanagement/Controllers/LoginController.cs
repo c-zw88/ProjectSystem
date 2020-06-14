e388a6cfc53d421e6d179b607c03e8defc33a795
c-zw88 <czw991117>
2020-06-14 23:22:21 +0800

    部门管理

--- 毕业项目代码数据库/Freemanagement/Worry-freemanagement/Controllers/LoginController.cs
+++ 毕业项目代码数据库/Freemanagement/Worry-freemanagement/Controllers/LoginController.cs
@@ -3,17 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Worry_freemanagement.Models;
 
 namespace Worry_freemanagement.Controllers
 {
     public class LoginController : Controller
     {
+        Entities db = new Entities();
         // GET: Login
         public ActionResult Index()
         {
             return View();
         }
         public ActionResult Department()
+        {
+            List<Departmental> list  = db.Departmental.ToList();
+            return View(list);
+        }
+        public ActionResult Add()
         {
             return View();
         }
